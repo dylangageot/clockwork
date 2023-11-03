3284725360b392b83af65aaf07d7e103be5b1310
Dylan Gageot <gageot.dylan@gmail.com>
2023-11-03 09:45:36 +0100

    Add top level with instruction memory in BRAM

--- top_level.vhd
+++ top_level.vhd
@@ -38,14 +38,19 @@ end top_level;
 architecture Behavioral of top_level is
 
 	component cpu is
-		 Port ( clk : in  STD_LOGIC;
-				  rst : in STD_LOGIC;
-				  address : inout  STD_LOGIC_VECTOR (31 downto 0);
-				  data : inout  STD_LOGIC_VECTOR (31 downto 0);
-				  wr : inout STD_LOGIC_VECTOR(3 downto 0);
-				  rd : inout STD_LOGIC;
-				  ready: inout STD_LOGIC
-				 );
+		 Port (
+				clk : in  STD_LOGIC;
+				rst : in STD_LOGIC;
+				enable : in STD_LOGIC;
+				daddress : inout  STD_LOGIC_VECTOR (31 downto 0);
+				ddata : inout  STD_LOGIC_VECTOR (31 downto 0);
+				dwr : inout STD_LOGIC_VECTOR(3 downto 0);
+				drd : inout STD_LOGIC;
+				dready: inout STD_LOGIC;
+				iaddress : out STD_LOGIC_VECTOR (31 downto 0);
+				idata : in STD_LOGIC_VECTOR (31 downto 0);
+				iready: in STD_LOGIC
+		 );
 	end component;
 
 	component clock_gen
@@ -67,11 +72,24 @@ architecture Behavioral of top_level is
 	  );
 	END COMPONENT;
 
+
+	component instruction_cache is
+		Port (
+			clka : IN STD_LOGIC;
+			ena : IN STD_LOGIC;
+			addra : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			douta : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+		);
+	end component;
+
 	signal clk_40m: std_logic := '0';
 	signal gpio: std_logic_vector (31 downto 0) := (others => '0');
-	signal rd, ready : std_logic := 'Z';
-	signal data, address, previous_address : std_logic_vector (31 downto 0) := (others => 'Z');
-	signal wr : std_logic_vector (3 downto 0) := (others => 'Z');
+	signal drd, dready : std_logic := 'Z';
+	signal ddata, daddress, previous_daddress : std_logic_vector (31 downto 0) := (others => 'Z');
+	signal dwr : std_logic_vector (3 downto 0) := (others => 'Z');
+
+	signal idata, iaddress, previous_iaddress : std_logic_vector (31 downto 0) := (others => 'Z');
+	signal iready : std_logic := '0';
 
    signal mem_out : std_logic_vector(31 downto 0);
 	signal enable_gpio, enable_mem, mem_ready : std_logic := '0';
@@ -79,13 +97,17 @@ architecture Behavioral of top_level is
 begin
 	
 	cpu1: cpu port map (
-		clk => clk_40m,
-		rst => rst,
-		address => address,
-		data => data,
-		wr => wr,
-		rd => rd,
-		ready => ready
+		 clk => clk_40m,
+		 rst => rst,
+		 enable => '1',
+		 daddress => daddress,
+		 ddata => ddata,
+		 dwr => dwr,
+		 drd => drd,
+		 dready => dready,
+		 iaddress => iaddress,
+		 idata => idata,
+		 iready => iready
 	);
 	
 	cg1: clock_gen port map (
@@ -96,50 +118,74 @@ begin
 	led <= gpio(7 downto 0);
 
 	--! GPIO
-	enable_gpio <= address(31);
-	ready <= '1' when enable_gpio = '1' and (rd = '1' or wr(0) = '1') else 'Z';
-	data <= gpio when enable_gpio = '1' and rd = '1' else (others => 'Z');
-	gpio_gen: process (clk_40m, rst, enable_gpio, wr)
+	enable_gpio <= daddress(31);
+	dready <= '1' when enable_gpio = '1' and (drd = '1' or dwr(0) = '1') else 'Z';
+	ddata <= gpio when enable_gpio = '1' and drd = '1' else (others => 'Z');
+	gpio_gen: process (clk_40m, rst, enable_gpio, dwr)
 	begin
 		if rst = '1' then
 			gpio <= (others => '0');
-		elsif rising_edge(clk_40m) and enable_gpio = '1' and wr /= "0000" then
-			gpio <= data;
+		elsif rising_edge(clk_40m) and enable_gpio = '1' and dwr /= "0000" then
+			gpio <= ddata;
 		end if;
 	end process;
 	
 	--! data cache
-	enable_mem <= not(address(31));
+	enable_mem <= not(daddress(31));
 	dc1: data_cache port map (
 		clka => clk_40m,
 		ena => enable_mem,
-		wea => wr,
-		addra => address,
-		dina => data,
+		wea => dwr,
+		addra => daddress,
+		dina => ddata,
 		douta => mem_out
 	);
 	
-	ready <= mem_ready when enable_mem = '1' and (wr(0) = '1' or rd = '1') else 'Z';
-	data <= mem_out when enable_mem = '1' and rd = '1' else (others => 'Z');
+	dready <= mem_ready when enable_mem = '1' and (dwr(0) = '1' or drd = '1') else 'Z';
+	ddata <= mem_out when enable_mem = '1' and drd = '1' else (others => 'Z');
 	
-	mem_ready_gen: process (wr, rd, address, previous_address)
+	mem_ready_gen: process (dwr, drd, daddress, previous_daddress)
 	begin
-		if wr(0) = '1' then
+		if dwr(0) = '1' then
 			mem_ready <= '1';
-		elsif rd = '1' and previous_address = address then
+		elsif drd = '1' and previous_daddress = daddress then
 			mem_ready <= '1';
 		else
 			mem_ready <= '0';
 		end if;
 	end process;
 	--! the memory shall throw a non-ready state once a new address is requested
-	previous_address_gen: process (clk_40m, address, wr, enable_mem)
+	previous_address_gen: process (clk_40m, daddress, dwr, enable_mem)
 	begin
 		if rising_edge(clk_40m) and enable_mem = '1' then
-			previous_address <= address;
+			previous_daddress <= daddress;
+		end if;
+	end process;
+
+	ic1: instruction_cache port map (
+		clka => clk_40m,
+		ena => '1',
+		addra => iaddress,
+		douta => idata
+	);
+	
+	iready_gen: process (iaddress, previous_iaddress)
+	begin
+		if previous_iaddress = iaddress then
+			iready <= '1';
+		else
+			iready <= '0';
 		end if;
 	end process;
 	
+	previous_iaddress_gen: process (rst, clk_40m, iaddress)
+	begin
+		if rst = '1' then
+			previous_iaddress <= (others => '0');
+		elsif rising_edge(clk_40m) then
+			previous_iaddress <= iaddress;
+		end if;
+	end process;
 	
 
 end Behavioral;
